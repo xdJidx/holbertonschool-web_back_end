ab8a6286d6843c41cc9df27f0ac45d3297b95a9e
xdJidx <bernardjerome59@gmail.com>
2023-08-28 09:30:04 +0200

    Correction pycodestyle and add arguement

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -3,5 +3,15 @@
 Basic annotations - add
 """
 
-def add(a :float, b : float) -> float:
+
+def add(a: float, b: float) -> float:
+    """Addition
+
+    Args:
+        a (float): Number to add at b
+        b (float): Number to add at a
+
+    Returns:
+        float: Number addition of a and b
+    """
     return (a + b)
