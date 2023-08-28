ab62f8bdf782ae9476ac36ac111840a59334f406
xdJidx <bernardjerome59@gmail.com>
2023-08-28 09:48:24 +0200

    Add file task 1 : Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concat
+
+    Args:
+        str1 (str): string on the left
+        str2 (str): string on the rigth
+
+    Returns:
+        str: 
+    """
+    return (str1 + str2)
