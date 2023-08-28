b487751ccc68578f4b22405129b6f5afd3a9f100
xdJidx <bernardjerome59@gmail.com>
2023-08-28 14:03:53 +0200

    Change name retunr if def line

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-2. Measure the runtime
+3. Tasks
 """
 import asyncio
 from typing import Awaitable
@@ -9,7 +9,7 @@ from typing import Awaitable
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Awaitable[float]:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """task_wait_random
        Creates and returns an asyncio.Task for wait_random coroutine
     Args:
@@ -18,4 +18,5 @@ def task_wait_random(max_delay: int) -> Awaitable[float]:
     Returns:
         Awaitable[float]: Create awaitable float
     """
+
     return asyncio.create_task(wait_random(max_delay))
