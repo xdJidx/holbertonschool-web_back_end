430f1073e6c32593cdeb21b16974e74f28d7bd20
xdJidx <bernardjerome59@gmail.com>
2023-08-28 10:06:50 +0200

    Add summury and correction pycodestyle

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-a : int = 1
-pi : float = 3.14
-i_understand_annotations : bool = True
-school : str = "Holberton"
+"""
+Define and annotate the following
+variables with the specified values
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
