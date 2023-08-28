bd0099cb2ce90fcd53768ed617ab5e943f7283d9
xdJidx <bernardjerome59@gmail.com>
2023-08-28 09:55:39 +0200

    Add file task 2 : Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - floor
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """floor
+
+    Args:
+        n (float) : float to become floor
+
+    Returns:
+        int : the floor of n
+    """
+    return math.floor(n)
