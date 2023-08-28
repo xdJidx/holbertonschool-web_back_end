ea0f29d7c2b92a59a65a0b7caeb0f6011bfad8d3
xdJidx <bernardjerome59@gmail.com>
2023-08-28 09:49:49 +0200

    Correction pycodestyle

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -12,6 +12,6 @@ def concat(str1: str, str2: str) -> str:
         str2 (str): string on the rigth
 
     Returns:
-        str: 
+        str: New string with str1 and str2
     """
     return (str1 + str2)
