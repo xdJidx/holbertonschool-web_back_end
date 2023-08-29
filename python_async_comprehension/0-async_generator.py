9d5afef2e81d9f5d2204ee4c9eb1169fe73cef31
xdJidx <bernardjerome59@gmail.com>
2023-08-29 09:19:00 +0200

    Correction pycodestyle, add comment in def and add typing return

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,11 +2,17 @@
 """
 0. Async Generator
 """
-import asyncio, random
+import asyncio
+import random
+from typing import List
 
 
-async def async_generator():
+async def async_generator() -> List[float]:
+    """async_generator
 
+    Yields:
+        float: Random number
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
