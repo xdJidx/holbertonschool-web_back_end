c300a84ea0c14f2fd403757aa8c15c0875a2a706
xdJidx <bernardjerome59@gmail.com>
2023-09-05 09:41:47 +0200

    Change type return

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,9 +4,10 @@ return a tuple of size two containing a start index and an end
 index corresponding to the range of indexes to return in a list
 for those particular pagination parameters.
 """
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """index range
 
     Args:
