f0d7f17c1988d95a7446f6fc3b824c6883fcb43a
xdJidx <bernardjerome59@gmail.com>
2023-09-05 10:52:43 +0200

    Change return def get_hyper

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -4,7 +4,7 @@
 """
 import csv
 import math
-from typing import List
+from typing import List, Dict
 from typing import Tuple
 
 
@@ -58,7 +58,7 @@ class Server:
             return []
         return dataset[start_index:end_index]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> {}:
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         data = self.get_page(page, page_size)
         total_pages = math.ceil(len(self.dataset()) / page_size)
 
