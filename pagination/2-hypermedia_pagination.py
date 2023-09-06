650119d453762cf399b735f9ae88d76daf9b0d07
xdJidx <bernardjerome59@gmail.com>
2023-09-06 10:27:34 +0200

    Change return and add conditon in

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -72,14 +72,11 @@ class Server:
         data = self.get_page(page, page_size)
         total_pages = math.ceil(len(self.dataset()) / page_size)
 
-        next_page = page + 1 if page < total_pages else None
-        prev_page = page - 1 if page > 1 else None
-
         return {
             "page_size": len(data),
             "page": page,
             "data": data,
-            "next_page": next_page,
-            "prev_page": prev_page,
+            "next_page": page + 1 if page < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
             "total_pages": total_pages,
         }
