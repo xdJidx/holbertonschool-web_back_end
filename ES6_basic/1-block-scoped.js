4c89ed886fab94536f7d541a95297b8900d2c391
xdJidx <bernardjerome59@gmail.com>
2023-09-12 15:14:59 +0200

    Change let in const

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
     var task2 = true;
   
     if (trueOrFalse) {
-      let task = true; // Use let instead of var
-      let task2 = false; // Use let instead of var
+      const task = true; // Use let instead of var
+      const task2 = false; // Use let instead of var
     }
   
     return [task, task2];
