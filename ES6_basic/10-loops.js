94acec3527f2d6ebdc0186641a9b2db1df2d52ed
xdJidx <bernardjerome59@gmail.com>
2023-09-13 10:59:41 +0200

    Add file task 10 : For...of Loops

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+    for (let idx in array) {
+      array[idx] = appendString + array[idx];
+    }
+  
+    return array;
+  }
+  
