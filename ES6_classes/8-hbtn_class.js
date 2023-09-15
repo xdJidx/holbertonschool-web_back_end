3378fdd5a6790ee2f9e5a334660104c99ba108f2
xdJidx <bernardjerome59@gmail.com>
2023-09-15 15:30:15 +0200

    Add file task 8 : Primitive - Holberton Class

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,26 @@
+export default class HolbertonClass {
+    constructor(size, location) {
+    this._size = size;
+    this._location = location;
+    }
+
+    // Getter for the 'size' attribute.
+    get size() {
+    return this._size;
+    }
+
+    // Getter for the 'location' attribute.
+    get location() {
+    return this._location;
+    }
+
+    // Define a custom casting behavior to Number.
+    valueOf() {
+    return this._size;
+    }
+
+    // Define a custom casting behavior to String.
+    toString() {
+    return this._location;
+    }
+}
