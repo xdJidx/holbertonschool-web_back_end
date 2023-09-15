9e1089e4868923c72f2163c58962d2c751f50f2f
xdJidx <bernardjerome59@gmail.com>
2023-09-15 15:28:02 +0200

    Add if conditon typeof

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,5 +1,12 @@
 export default class Airport {
   constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a String');
+    }
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a String');
+    }
+
     this._name = name;
     this._code = code;
   }
