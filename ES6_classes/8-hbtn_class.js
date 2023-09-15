b9f10e51f5cf32a72d17e36f37f47afd338de08d
xdJidx <bernardjerome59@gmail.com>
2023-09-15 15:31:34 +0200

    Fix lint

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,26 +1,26 @@
 export default class HolbertonClass {
-    constructor(size, location) {
+  constructor(size, location) {
     this._size = size;
     this._location = location;
-    }
+  }
 
-    // Getter for the 'size' attribute.
-    get size() {
+  // Getter for the 'size' attribute.
+  get size() {
     return this._size;
-    }
+  }
 
-    // Getter for the 'location' attribute.
-    get location() {
+  // Getter for the 'location' attribute.
+  get location() {
     return this._location;
-    }
+  }
 
-    // Define a custom casting behavior to Number.
-    valueOf() {
+  // Define a custom casting behavior to Number.
+  valueOf() {
     return this._size;
-    }
+  }
 
-    // Define a custom casting behavior to String.
-    toString() {
+  // Define a custom casting behavior to String.
+  toString() {
     return this._location;
-    }
+  }
 }
