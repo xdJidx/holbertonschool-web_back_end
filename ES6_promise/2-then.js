3e1d4a746d0a17ad4470144f2acd60b3954788ea
xdJidx <bernardjerome59@gmail.com>
2023-10-02 15:43:58 +0200

    Correction lint

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,9 +1,9 @@
 // 2-then.js
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({
-      status: 200,
-      body: 'success'
-    }))
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
     .catch(() => new Error());
 }
