d453152e31265b1863907fcb600270f6de93f781
xdJidx <bernardjerome59@gmail.com>
2023-10-02 16:02:40 +0200

    Add file task 3 : Handle multiple successful promises

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+// 3-all.js
+import { uploadPhoto, createUser } from "./utils";
+
+export default async function handleProfileSignup() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  } catch (error) {
+    console.error('Signup system offline');
+  }
+}
