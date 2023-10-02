4fa47da852fde188a0d0a72cad062fa71ced570a
xdJidx <bernardjerome59@gmail.com>
2023-10-02 14:49:17 +0200

    Add file task 0 : Keep every promise you make and only make promises you can keep and README not empty

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,12 @@
+// 0-promise.js
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      // Simulate an API call or any asynchronous operation
+      setTimeout(() => {
+        // Resolve the Promise with some data
+        resolve('API response data');
+        // You can also reject the Promise in case of an error
+        // reject('API request failed');
+      }, 1000); // Simulating a delay of 1 second
+    });
+  }
---
+++ ES6_promise/README.md
@@ -0,0 +1,5 @@
+# ES6 Promises
+
+## Tasks
+
+### 0. Keep every promise you make and only make promises you can keep
