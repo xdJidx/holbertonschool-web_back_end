2c7b7e3800586b5676e4e2bee3dc4f9d7c358a1a
xdJidx <bernardjerome59@gmail.com>
2023-10-02 15:21:38 +0200

    Add file task 2 : Catch me if you can!

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+// 2-then.js
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      return new Error();
+    });
+}
