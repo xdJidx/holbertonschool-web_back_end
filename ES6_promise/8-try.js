be3f4413bdbdf636bdbca84e7877f6ef1079b604
xdJidx <bernardjerome59@gmail.com>
2023-10-03 10:39:07 +0200

    Add file task 8 : Throw error / try catch

---
+++ ES6_promise/8-try.js
@@ -0,0 +1,7 @@
+// 8-try.js
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  return numerator / denominator;
+}
