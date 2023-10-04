c67887d570e5e0e42e43b451568b1982e8de3e83
xdJidx <bernardjerome59@gmail.com>
2023-10-04 11:22:02 +0200

    Lint correction

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
-    throw new Error("Position outside range");
+    throw new Error('Position outside range');
   }
   // Create a new ArrayBuffer with the specified length
   const buffer = new ArrayBuffer(length);
