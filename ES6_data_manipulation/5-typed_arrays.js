152a9bb66e3490e50f5d9ce1e7612e4a74f4621f
xdJidx <bernardjerome59@gmail.com>
2023-10-04 11:20:51 +0200

    Add file task 5 : Typed Arrays

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,15 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error("Position outside range");
+  }
+  // Create a new ArrayBuffer with the specified length
+  const buffer = new ArrayBuffer(length);
+
+  // Create a DataView to work with the ArrayBuffer
+  const view = new DataView(buffer);
+
+  // Set the Int8 value at the specified position
+  view.setInt8(position, value);
+
+  return view;
+}
