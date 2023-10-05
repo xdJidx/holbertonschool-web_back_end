d4da711c95751243a2c8e718902285cabeb5cf26
xdJidx <bernardjerome59@gmail.com>
2023-10-05 11:02:09 +0200

    Add file task 8 : Clean set

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,20 @@
+export default function cleanSet(set, startString) {
+  const string = [];
+
+  if (
+    typeof set !== 'object'
+        || typeof startString !== 'string'
+        || startString.length === 0) {
+    return '';
+  }
+
+  for (const value of set) {
+    // Check if the current value starts with startString
+    if (value.startsWith(startString)) {
+      // If yes, add the rest of the string (excluding startString) to the matchingValues array
+      string.push(value.slice(startString.length));
+    }
+  }
+
+  return string.join('-');
+}
