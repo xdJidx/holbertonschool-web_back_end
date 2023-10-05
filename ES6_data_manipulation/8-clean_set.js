2b5a385ab3fd6d610a31f91f2c5f588d82c69680
xdJidx <bernardjerome59@gmail.com>
2023-10-05 11:07:48 +0200

    Correction condition if

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -10,7 +10,7 @@ export default function cleanSet(set, startString) {
 
   for (const value of set) {
     // Check if the current value starts with startString
-    if (value.startsWith(startString)) {
+    if (value && value.startsWith(startString)) {
       // If yes, add the rest of the string (excluding startString) to the matchingValues array
       string.push(value.slice(startString.length));
     }
