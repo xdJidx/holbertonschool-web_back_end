578d9466cb0023b4427171f1af9033a40c572adc
xdJidx <bernardjerome59@gmail.com>
2024-02-05 10:20:10 +0100

    Change condition of position before

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -18,9 +18,10 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
-        self.cache_data[key] = item
-        if key or item is None:
+        if key is None or item is None:
             return
+        self.cache_data[key] = item
+
 
     def get(self, key):
         """ Get an item by key
