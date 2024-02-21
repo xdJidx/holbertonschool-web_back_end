6412b968b7f71e97b4595b89e68c5b9a6eb5908c
xdJidx <bernardjerome59@gmail.com>
2024-02-21 14:37:55 +0100

    Correction pycodestyle and documented file

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -5,11 +5,20 @@ Use bcrypt to hash a password
 
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
+    """ Returns a salted, hashed password, which is a byte string.
+
+    Args:
+        password (str): The password to hash.
+
+    Returns:
+        bytes: The hashed password.
+    """
     # Generate a random salt
     salt = bcrypt.gensalt()
-    
+
     # Hash the password with the generated salt
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    
+
     return hashed_password
