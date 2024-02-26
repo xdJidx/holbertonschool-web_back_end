069106bf0ceb8bab186027346567e7e6e8f982b1
xdJidx <bernardjerome59@gmail.com>
2024-02-26 09:04:05 +0100

    Add README and user model

---
+++ user_authentication_service/user.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""0. User model
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    """User class
+
+    Args:
+        Base (declarative_base): Base class
+
+    Returns:
+        User: User class
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
