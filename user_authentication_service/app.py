d24cfbfc3d8176685ebd5ae385bf114eb485e625
xdJidx <bernardjerome59@gmail.com>
2024-02-29 10:11:29 +0100

    pycodestyle

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -6,9 +6,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def welcome():
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
