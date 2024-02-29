d30909b1eb8d63838d517bdb64847a52b1ccf910
xdJidx <bernardjerome59@gmail.com>
2024-02-29 10:17:51 +0100

    Register user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,15 +2,30 @@
 """Main file
 """
 
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
-@app.route("/")
-def welcome():
-    return jsonify({"message": "Bienvenue"})
+@app.route('/users', methods=['POST'])
+def users():
+    try:
+        # Extract email and password from the form data
+        email = request.form['email']
+        password = request.form['password']
+
+        # Register the user using the AUTH object
+        user = AUTH.register_user(email, password)
+
+        # Respond with a JSON payload for successful registration
+        return jsonify({"email": user.email, "message": "user created"}), 200
+
+    except ValueError as e:
+        # Catch the exception for already registered user
+        return jsonify({"message": "email already registered"}), 400
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
