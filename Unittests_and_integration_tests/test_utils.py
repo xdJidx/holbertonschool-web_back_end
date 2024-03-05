0fd4720f7373b9bc6de23e07ff7e328065ac008c
xdJidx <bernardjerome59@gmail.com>
2024-03-05 09:37:34 +0100

    Change format assertEqual

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -4,29 +4,49 @@
 
 import unittest
 from parameterized import parameterized
-import utils
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """ Access nested map """
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, nested_map, path, expected_result):
-        self.assertEqual(utils.access_nested_map(nested_map, path),
-                         expected_result)
+    def test_access_nested_map(self, nested_map, path_map, result_expec):
+        """ Access nested method
+
+            args:
+                nested_map: {"a"} : Le dict. dans lequel accéder à la valeur
+                path: ("a",) : Le chemin pour accéder au dict.
+                result_expec: 1 : Le résultat attendu après l'accès à la valeur
+
+            return
+                Ok if its correct
+        """
+        self.assertEqual(access_nested_map(nested_map, path_map), result_expec)
 
     @parameterized.expand([
-        ({}, ("a",), "KeyError for path ('a',) not raised"),
-        ({"a": 1}, ("a", "b"), "KeyError for path ('a', 'b') not raised"),
+        ({}, ("a")),
+        ({"a": 1}, ("a", "b"))
     ])
+    def test_access_nested_map_exception(self, nested_map, path_map):
+        """ Exception access nested method
 
-    def test_access_nested_map_exception(self, nested_map, path, expected_error_message):
-        with self.assertRaises(KeyError) as context:
-            utils.access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), expected_error_message)
+            args:
+                nested_map: {}
+                path: ("a",)
+
+            return:
+                ok if its correct
+        """
+        with self.assertRaises(KeyError) as error:
+            access_nested_map(nested_map, path_map)
+
+        self.assertEqual(
+            f'KeyError({str(error.exception)})', repr(error.exception))
 
 
 if __name__ == '__main__':
