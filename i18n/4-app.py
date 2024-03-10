f2c74fb79d0312adef96f087d769f5d4f34b59f4
xdJidx <bernardjerome59@gmail.com>
2024-03-10 19:58:31 +0100

    Correction function

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""task 4"""
+""" Basic Babel setup """
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
@@ -8,7 +8,7 @@ babel = Babel(app)
 
 
 class Config(object):
-    """Config class to setup Babel for English and French"""
+    """ Configuration Babel """
     LANGUAGES = ["en", "fr"]
     Babel.default_locale = "en"
     Babel.default_timezone = "UTC"
@@ -19,13 +19,21 @@ app.config.from_object(Config)
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def index() -> str:
-    """ Template for 4-index """
+    """ Greeting
+
+        Return:
+            Initial template html
+    """
     return render_template('4-index.html')
 
 
 @babel.localeselector
 def get_locale():
-    """ Locale language selector function """
+    """ Locale language
+
+        Return:
+            Best match to the language
+    """
     user_locale = request.args.get('locale')
     if user_locale in app.config['LANGUAGES']:
         return user_locale
