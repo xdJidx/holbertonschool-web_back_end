720baf59aef7081ce39461ee3455f91dd7531102
xdJidx <bernardjerome59@gmail.com>
2024-03-18 14:16:26 +0100

    Writing strings to Redis

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Module for testing client.py """
+
+import redis
+import uuid
+from typing import Union
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
