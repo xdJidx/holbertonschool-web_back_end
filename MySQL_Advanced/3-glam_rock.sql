394796bb06a7a91c9f3459c062cb84205dbceec3
xdJidx <bernardjerome59@gmail.com>
2024-03-21 10:35:47 +0100

    Old school band

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- Select the band name and the difference between the earliest formed year
+-- and the latest split year as lifespan
+SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
