fc8c73f1c83298c897137057f3c6f9b374258496
xdJidx <bernardjerome59@gmail.com>
2024-03-21 10:31:39 +0100

    Try change name and add new comment

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,5 +1,6 @@
 -- Make a lsit of best band fans
-SELECT origin AS origin ,SUM(fans) AS nb_Fans
+-- 2. Select the origin and the sum of fans from the metal_bands table.
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
-ORDER BY nb_Fans DESC;
+ORDER BY nb_fans DESC;
