39b8b3bba6632959b43b25a6dbb883ff01f6df12
xdJidx <bernardjerome59@gmail.com>
2024-03-22 10:17:43 +0100

    Try to delete database to correct

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,20 +1,15 @@
--- Create a stored procedure named `ComputeAverageScoreForUser` 
--- that takes a user_id as input and computes the average score for that user.
+-- Stored procedure to compute and store the average score for a student
+-- Input: user_id, a users.id value
 DELIMITER //
-
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT) -- Paramètre d'entrée représentant l'identifiant de l'utilisateur
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    -- Déclaration d'une variable pr stocker la moyenne des scores
-    DECLARE avg_score FLOAT;
-
-    -- Calcul de la moyenne des scores pour l'utilisateur spécifié
-    SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
-
-    -- Update de la colonne average_score dans la table users avec la moyenne calculée
-    UPDATE users SET average_score = avg_score WHERE id=user_id;
-END
-
-END;
-//
+    UPDATE users
+    SET average_score = (
+        SELECT AVG(score)
+        FROM corrections
+        WHERE user_id = users.id
+    )
+    WHERE users.id = user_id;
+END//
 DELIMITER ;
