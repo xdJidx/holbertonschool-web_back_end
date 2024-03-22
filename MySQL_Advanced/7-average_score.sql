07007ffe274f4e97f61b33441a73413fc23b3655
xdJidx <bernardjerome59@gmail.com>
2024-03-22 09:22:40 +0100

    Test new code

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -3,47 +3,18 @@
 DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
-)
+    IN user_id INT) -- Paramètre d'entrée représentant l'identifiant de l'utilisateur
 BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE total_corrections INT;
-    DECLARE current_average FLOAT;
+    -- Déclaration d'une variable pr stocker la moyenne des scores
+    DECLARE avg_score FLOAT;
 
-    -- Calculate total score for the user
-    SELECT SUM(score) INTO total_score
-    FROM corrections
-    WHERE user_id = user_id;
+    -- Calcul de la moyenne des scores pour l'utilisateur spécifié
+    SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
 
-    -- Calculate total number of corrections for the user
-    SELECT COUNT(*) INTO total_corrections
-    FROM corrections
-    WHERE user_id = user_id;
+    -- Update de la colonne average_score dans la table users avec la moyenne calculée
+    UPDATE users SET average_score = avg_score WHERE id=user_id;
+END
 
-    -- Calculate current average score for the user
-    SELECT average_score INTO current_average
-    FROM users
-    WHERE id = user_id;
-
-    -- Compute new average score
-    IF total_corrections > 0 THEN
-        IF total_corrections = 1 THEN
-            -- If it's the first correction, just set the average to the score
-            UPDATE users
-            SET average_score = total_score
-            WHERE id = user_id;
-        ELSE
-            -- Otherwise, update the average by taking into account the previous average and the new score
-            UPDATE users
-            SET average_score = (current_average * (total_corrections - 1) + total_score) / total_corrections
-            WHERE id = user_id;
-        END IF;
-    ELSE
-        -- If there are no corrections, set the average to 0
-        UPDATE users
-        SET average_score = 0
-        WHERE id = user_id;
-    END IF;
 END;
 //
 DELIMITER ;
