a18dcc639266d6eeb9b164d358d2b7ecf72e4577
xdJidx <bernardjerome59@gmail.com>
2024-03-22 10:21:27 +0100

    Change number of decimal display

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,22 +1,15 @@
 -- Create a function that takes two integers and returns a float.
 -- If the second integer is 0, the function should return 0.
 DELIMITER //
-
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS FLOAT
-DETERMINISTIC
-NO SQL
+RETURNS INT
 BEGIN
-    DECLARE result FLOAT;
-    
+    DECLARE result INT;
     IF b = 0 THEN
         SET result = 0;
     ELSE
-        SET result = ROUND(a / b, 6);
+        SET result = a / b;
     END IF;
-    
     RETURN result;
-END;
-//
-
+END//
 DELIMITER ;
